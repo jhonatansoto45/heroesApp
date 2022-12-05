dd2d054a112cfa5748416e19a8e69da478ca90f5
jhonatan.soto <jhonatan.soto@bmslatinoamerica.com>
2022-12-05 11:35:03 -0500

    Refactor: Terminado sidenav

--- src/app/heroes/pages/home/home.component.ts
+++ src/app/heroes/pages/home/home.component.ts
@@ -3,13 +3,20 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: []
+  styles: [
+    `
+      a {
+        text-decoration: none;
+      }
+
+      .container {
+        margin: 10px;
+      }
+    `,
+  ],
 })
 export class HomeComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
