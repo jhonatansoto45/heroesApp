ef3257134249f61d749f4e98b8a942a9ea383bbe
jhonatan.soto <jhonatan.soto@bmslatinoamerica.com>
2023-01-11 11:14:41 -0500

    Buscador de heroe

--- src/app/heroes/pages/buscar/buscar.component.ts
+++ src/app/heroes/pages/buscar/buscar.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { MatAutocompleteSelectedEvent } from '@angular/material/autocomplete';
+import { Heroe } from '../../interfaces/heroes.interface';
+import { HeroesService } from '../../services/heroes.service';
 
 @Component({
   selector: 'app-buscar',
@@ -6,7 +9,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: [],
 })
 export class BuscarComponent implements OnInit {
-  constructor() {}
+  termino: string = '';
+  heroes: Heroe[] = [];
+  heroeSeleccionado: Heroe | undefined;
+
+  notFound: boolean = false;
+
+  constructor(private heroesService: HeroesService) {}
 
   ngOnInit(): void {}
+
+  buscando(): void {
+    this.heroesService
+      .getSugerencias(this.termino.trim())
+      .subscribe((heroes) => (this.heroes = heroes));
+  }
+
+  opcionSeleccionada(event: MatAutocompleteSelectedEvent): void {
+    if (!event.option.value) {
+      this.heroeSeleccionado = undefined;
+      return;
+    }
+
+    const heroe: Heroe = event.option.value;
+    this.termino = heroe.superhero;
+
+    this.heroesService
+      .getHeroePorId(heroe.id!) //* SIEMPRE TENDRA UN VALOR
+      .subscribe((heroe) => (this.heroeSeleccionado = heroe));
+  }
 }
